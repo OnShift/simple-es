0a513277a613f0f1e4ed4ed8927ebd6012c335ab
Bryan Dodd <aqualad@gmail.com>
2017-03-23 05:46:00 -0400

    Rename src to match project name

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     name="SimpleES",
     version="0.1.0",
     description='A simple Event Sourcing library for Python',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=find_packages(),
     setup_requires=['pytest-runner=2.9'],
     tests_require=['pytest==2.9.2']
 )
