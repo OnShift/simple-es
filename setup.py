3f889751612372a7725104e9b8d4bdc20bd29fe9
Bryan Dodd <aqualad@gmail.com>
2017-03-23 05:46:24 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SimpleES",
-    version="0.1.0",
+    version="0.2.0",
     description='A simple Event Sourcing library for Python',
     packages=find_packages(),
     setup_requires=['pytest-runner=2.9'],
