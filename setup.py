db0b394ffc9bc49c2cd1c65e4a41581c7f4c03cd
Bryan Dodd <aqualad@gmail.com>
2017-03-31 23:22:20 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SimpleES",
-    version="0.6.0",
+    version="0.7.0",
     description='A simple Event Sourcing library for Python',
     packages=find_packages()
 )
