84fbda722e979d48a5212472c18cedba0a3c03d3
Bryan Dodd <aqualad@gmail.com>
2017-04-13 16:56:14 -0400

    Update setup to reflect file restructure

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="SimpleES",
-    version="0.8.0",
+    version="0.9.0",
     description='A simple Event Sourcing library for Python',
-    packages=find_packages(),
-    package_dir={'': '.'}
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
 )
